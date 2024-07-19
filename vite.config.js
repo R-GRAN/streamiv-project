4de644000b60759782459fd66404ab7fcc70b5e8
Remy <remy.grangenois@gmail.com>
2024-07-19 16:47:01 +0200

    desktop header

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  resolve: { alias: { "@": "src" } },
+  resolve: { alias: { "@": "/src" } },
 });
